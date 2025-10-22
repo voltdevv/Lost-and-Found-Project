29146f967c691387abb9d25c8d8bbea861f931cf
zadam <zerenadam7@gmail.com>
2025-10-21 23:18:04 -0400

    started database

--- backend/db.py
+++ backend/db.py
@@ -2,9 +2,23 @@ import sqlite3
 
 class Database:
 
-    def __init__(self):
-        self.con = sqlite3.connect('Lost_and_Found.db')
+    def __init__(self, db_name):
+        self.con = sqlite3.connect(f'{db_name}.db')
         self.cursor = self.con.cursor()
+
+    def setup(self, table_name, *values):
+        vals = ", ".join(values)
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({vals})")
+        self.con.commit()
+
+    def add_item(self, *values):
+        pass
+
+    def fetch_all(self, table_name):
+
+        self.cursor.execute(f"SELECT * FROM {table_name}")
+        return self.cursor.fetchall()
+
 
 
     
